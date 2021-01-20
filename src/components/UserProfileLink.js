be41233901a6ce34cf877ebaf3ac8882e9616a96
jammychiou1 <jammy.chiou1@gmail.com>
2021-01-20 23:35:19 +0800

    link color is now always info.dark (blue)

--- src/components/UserProfileLink.js
+++ src/components/UserProfileLink.js
@@ -1,14 +1,24 @@
-import { Link as RouterLink} from 'react-router-dom';
-import { Link, Typography } from '@material-ui/core';
+import { Link as RouterLink } from 'react-router-dom';
+import { Link, makeStyles } from '@material-ui/core';
 
 import { make_user_profile_route } from '../constants';
 
+
+const useStyles = makeStyles(theme => ({
+  link: {
+    color: theme.palette.info.dark
+  }
+}));
+
 function UserProfileLink({ username }) {
+  const classes = useStyles();
+
   return (
     <Link 
       component={RouterLink} 
       to={make_user_profile_route(username)}
       onClick={e => e.stopPropagation()}
+      className={classes.link}
     >
       {username}
     </Link>
